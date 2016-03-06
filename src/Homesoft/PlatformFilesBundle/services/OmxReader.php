14c4198a2067f5a99ca8a6f67cea6b0d53ff8cfa
tompouce <thomaspeyrou@gmail.com>
2016-03-06 14:17:00 +0100

    Amélioration views files

--- src/Homesoft/PlatformFilesBundle/services/OmxReader.php
+++ src/Homesoft/PlatformFilesBundle/services/OmxReader.php
@@ -4,13 +4,15 @@ namespace Homesoft\PlatformFilesBundle\services;
 
 
 class OmxReader {
-
+    private $fifoFile = "/tmp/cmd-omxplayer";
     public function link($file) {
         if($this->fifoExist()){
             $this->removeFifo();
         }
         $this->createFifo();
-        $cmd = 'omxplayer -o hdmi --blank ' . $file . " < /tmp/cmd-omxplayer";
+        //$cmd = 'omxplayer -o hdmi --blank ' . $file . " < " . $this->fifoFile;
+        $cmd = 'mplayer -vo x11 -slave -input file='. $this->fifoFile.' '.$file;
+        echo $cmd;
         $msg = shell_exec($cmd);
         sleep(1);
         return $msg;
@@ -18,70 +20,76 @@ class OmxReader {
 
     public function play() {
         sleep(5);
-        $cmd = "echo . > /tmp/cmd-omxplayer";
+        $cmd = 'echo . > '.$this->fifoFile;
         $msg = shell_exec($cmd);
         sleep(1);
         return $msg;
     }
 
     public function pause() {
-        $cmd = "echo -n p > /tmp/cmd-omxplayer";
+        $cmd = 'echo -n p > '.$this->fifoFile;
         $msg = shell_exec($cmd);
         return $msg;
     }
 
     public function rewind() {
-        $cmd = "echo -n $'\x5b\x44' > /tmp/cmd-omxplayer";
+        $cmd = 'echo -n $\'\x5b\x44\' > '.$this->fifoFile;
         $msg = shell_exec($cmd);
         sleep(1);
         return $msg;
     }
     public function stop() {
-        $cmd = "echo -n q > /tmp/cmd-omxplayer";
+        $cmd = 'echo -n q > '.$this->fifoFile;
         $msg = shell_exec($cmd);
         sleep(1);
         return $msg;
     }
 
     public function loadSubtitle() {
-        $msg = shell_exec('echo -n s > /tmp/cmd-omxplayer');
+        $msg = shell_exec('echo -n s > '.$this->fifoFile);
         sleep(1);
         return $msg;
     }
     public function forward() {
-        $cmd = "echo -n $'\x5b\x43' > /tmp/cmd-omxplayer";
+        $cmd = 'echo -n $\'\x5b\x43\' > '.$this->fifoFile;
         $msg = shell_exec($cmd);
         sleep(1);
         return $msg;
     }
     public function increaseVolume() {
-        $msg = shell_exec('echo -n + > /tmp/cmd-omxplayer');
+        $msg = shell_exec('echo -n + > '.$this->fifoFile);
         sleep(1);
         return $msg;
     }
     public function decreaseVolume() {
-        $msg = shell_exec('echo -n - > /tmp/cmd-omxplayer');
+        $msg = shell_exec('echo -n - > '.$this->fifoFile);
         sleep(1);
         return $msg;
     }
 
     public function fifoExist() {
-        if(file_exists("/tmp/cmd-omxplayer")) {
+        if(file_exists($this->fifoFile)) {
             return true;
         }
         return false;
     }
 
     public function createFifo() {
-        $cmd = 'mkfifo /tmp/cmd-omxplayer';
+        $cmd = 'mkfifo '.$this->fifoFile;
         $msg = shell_exec($cmd);
         sleep(1);
         return $msg;
     }
     public function removeFifo() {
-        $cmd = 'rm /tmp/cmd-omxplayer';
+        $cmd = 'rm ' . $this->fifoFile;
         $msg = shell_exec($cmd);
         sleep(1);
         return $msg;
+    }
+    public  function getFifoFile() {
+        return $this->fifoFile;
+    }
+    public function setFifoFile($fifoFile){
+        $this->fifoFile = $fifoFile;
     }
 }
