5949d4fd996b7a4b5ce79c664b73341eb840af77
tompouce <thomaspeyrou@gmail.com>
2016-04-16 12:45:27 +0200

    Add livereload npm

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,28 @@
-/**
- * Created by tompouce on 16/04/2016.
- */
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json'),
+        watch:{
+            options:{livereload:true},
+            files:[
+                '**/*.html.twig'
+            ],
+            tasks:[]
+        },
+        express:{
+            all:{
+                options:{
+                    port:3000,
+                    hostname:'localhost',
+                    bases:['./public'],
+                    livereload:true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express');
+    grunt.registerTask('server',['express','watch']);
+
+};
+
